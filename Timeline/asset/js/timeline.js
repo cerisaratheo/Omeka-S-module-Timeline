a8268ac751e4b02d51806021df4fe039aa1da0ef
cerisara theo <theo.cerisara8@etu.univ-lorraine.fr>
2020-05-20 14:45:56 +0200

    possibilite de changer les filtres lorsque le mode plein ecran est activé et possibilité de cacher ou de montrer la zone de filtre lorsque le mode plein ecran est activé

--- Timeline/asset/js/timeline.js
+++ Timeline/asset/js/timeline.js
@@ -183,36 +183,98 @@ function addFullPageButton(timelineId) {
 //
 
 
+function recurseDomChildren(start, position, display)
+{
+  var nodes;
+  if(start.children)
+  {
+    nodes = start.children;
+    loopNodeChildren(nodes, position, display);
+  }
+}
+
+function loopNodeChildren(nodes, position, display)
+{
+  var node;
+  for(var i=0;i<nodes.length;i++)
+  {
+    node = nodes[i];
+    //console.log("dddddddddddddddddddddd "+node);
+    node.style.position = position;
+    node.style.display = display;
+    if(node.children)
+    {
+      recurseDomChildren(node, position, display);
+    }
+  }
+}
+
+
 function setupInputFiltersHighlights(timeline, bandIndices, theme, table, handler) {
   window.regexpckb = [];
-  var tr = table.insertRow(2);
+
+  // la row0 a deja ete inseree: elle contient le bouton "clearall"
+  // la row1 contiendra le label Filter
+  var tr = table.insertRow(1);
   var td = tr.insertCell(0);
   td.style.borderBottomStyle = "none";
+  td.innerHTML = "Filter :";
 
+  // la row2 contiendra le textfield pour le filtre
+  tr = table.insertRow(2);
+  window.tr2 = tr;
+  td = tr.insertCell(0);
+  td.style.borderBottomStyle = "none";
   var input = document.createElement("input");
   input.type = "text";
+  input.id="filtreInput";
   SimileAjax.DOM.registerEvent(input, "keydown", handler);
   td.appendChild(input);
 
+  // la row3 contient le label "highlight"
+  tr = table.insertRow(3);
+  td = tr.insertCell(0);
+  window.hl = td;
+  td.style.borderBottomStyle = "none";
+  td.innerHTML = "Highlights:";
+
+  /*
+  td = tr.insertCell(1);
+  td.style.borderBottomStyle = "none";
+  var divhigh = document.createElement("div");
+  td.appendChild(divhigh);
+  */
+
+  // la row4 contient les N highights text field
+  tr = table.insertRow(4);
   for (var i = 0; i < theme.event.highlightColors.length; i++) {
-    td = tr.insertCell(i+1);
+    td = tr.insertCell(i);
     td.style.borderBottomStyle = "none";
+
+    /*
+    var divhighi = document.createElement("div");
+    divhigh.appendChild(divhighi);
+    */
 
     input = document.createElement("input");
     input.type = "text";
+    input.id="filtreInput";
     SimileAjax.DOM.registerEvent(input, "keydown", handler);
     td.appendChild(input);
+    //divhighi.appendChild(input);
 
     var divColor = document.createElement("div");
     divColor.style.height = "0.5em";
     divColor.style.background = theme.event.highlightColors[i];
     td.appendChild(divColor);
+    //divhighi.appendChild(divColor);
   }
 }
 
 
 function setupFilterHighlightControls(timeline, bandIndices, theme, params) {
   var tablelabels = document.createElement("table");
+  window.tb = tablelabels;
   var tableflt = document.createElement("table");
   var divlabels = document.getElementById("fltLabelsButton");
   var divflt = document.getElementById("ckbfilters");
@@ -241,14 +303,6 @@ function setupFilterHighlightControls(timeline, bandIndices, theme, params) {
   }
 
   if (params.userFilters === '0') {
-    tr = tablelabels.insertRow(1);
-    td = tr.insertCell(0);
-    td.style.borderBottomStyle = "none";
-    td.innerHTML = "Filter :";
-
-    td = tr.insertCell(1);
-    td.style.borderBottomStyle = "none";
-    td.innerHTML = "Highlight:";
     setupInputFiltersHighlights(timeline, bandIndices, theme, tablelabels, handlerInputs);
   }
 
@@ -332,9 +386,10 @@ function performFilteringInputs(timeline, bandIndices, table) {
     }
   }
 
+  tr = table.rows[4];
   var regexes = [];
   var hasHighlights = false;
-  for (var x = 1; x < tr.cells.length; x++) {
+  for (var x = 0; x < tr.cells.length; x++) {
     var input = tr.cells[x].firstChild;
     var text2 = cleanString(input.value);
     if (text2.length > 0) {
@@ -366,7 +421,8 @@ function performFilteringInputs(timeline, bandIndices, table) {
 
 function clearAllInputs(timeline, bandIndices, table) {
   window.regexpfilter = null;
-  var tr = table.rows[2];
+  table.rows[2].cells[0].firstChild.value = "";
+  var tr = table.rows[4];
   for (var x = 0; x < tr.cells.length; x++) {
     tr.cells[x].firstChild.value = "";
   }
@@ -419,31 +475,31 @@ function performFilteringCkb(timeline, bandIndices, table) {
     console.log("   obj    "+evt._id);
     console.log("   ClassName    "+evt._classname);
     if (evt._id === "e381") {
-      console.log("   evt     "+Object.values(evt));
-      console.log("   keys    "+Object.keys(evt));
-      console.log("   TRUC    "+evt._classname);
-    }
-    console.log("   obj    "+Object.keys(evt._obj));
-    */
-    if (window.regexpfilter==null && window.regexpckb.length==0) return true;
-    for (var i=0; i<window.regexpckb.length; i++) {
-      var regex = new RegExp(window.regexpckb[i], "i");
-      if (regex.test(evt.getText()) || regex.test(evt.getDescription()) || regex.test(evt.getStart()) || regex.test(evt.getClassName())) {
-        return true;
-      }
-    }
-    if (window.regexpfilter==null) return false;
-    {
-      var regex = new RegExp(window.regexpfilter, "i");
-      return regex.test(evt.getText()) || regex.test(evt.getDescription()) || regex.test(evt.getStart()) || regex.test(evt.getClassName());
+    console.log("   evt     "+Object.values(evt));
+    console.log("   keys    "+Object.keys(evt));
+    console.log("   TRUC    "+evt._classname);
+  }
+  console.log("   obj    "+Object.keys(evt._obj));
+  */
+  if (window.regexpfilter==null && window.regexpckb.length==0) return true;
+  for (var i=0; i<window.regexpckb.length; i++) {
+    var regex = new RegExp(window.regexpckb[i], "i");
+    if (regex.test(evt.getText()) || regex.test(evt.getDescription()) || regex.test(evt.getStart()) || regex.test(evt.getClassName())) {
+      return true;
     }
   }
+  if (window.regexpfilter==null) return false;
+  {
+    var regex = new RegExp(window.regexpfilter, "i");
+    return regex.test(evt.getText()) || regex.test(evt.getDescription()) || regex.test(evt.getStart()) || regex.test(evt.getClassName());
+  }
+}
 
-  for (var i = 0; i < bandIndices.length; i++) {
-    var bandIndex = bandIndices[i];
-    timeline.getBand(bandIndex).getEventPainter().setFilterMatcher(filterMatcher);
-  }
-  timeline.paint();
+for (var i = 0; i < bandIndices.length; i++) {
+  var bandIndex = bandIndices[i];
+  timeline.getBand(bandIndex).getEventPainter().setFilterMatcher(filterMatcher);
+}
+timeline.paint();
 }
 
 function clearAllCkb(timeline, bandIndices, table, createfilt) {
@@ -477,7 +533,58 @@ function clearAllCkb(timeline, bandIndices, table, createfilt) {
 //
 //
 
+function clickfleche() {
+  if (window.fltVisible===1) {
+    //console.log("visible 1 "+window.fltVisible);
+    if (screen.height < 1080) {
+      animateLeft(window.divfpfiltzone, 0, -24);
+    }
+    else {
+      animateLeft(window.divfpfiltzone, 0, -17);
+    }
+    window.fleche.setAttribute('src', '/omeka-s/modules/Timeline/asset/img/flecheDroite.svg');
+    window.fltVisible = 0;
+  }
+  else {
+    //console.log("visible 0 "+window.fltVisible);
+    if (screen.height < 1080) {
+      animateLeft(window.divfpfiltzone, -24, 0);
+    }
+    else {
+      animateLeft(window.divfpfiltzone, -17, 0);
+    }
+    window.fleche.setAttribute('src', '/omeka-s/modules/Timeline/asset/img/flecheGauche.svg');
+    window.fltVisible = 1;
+  }
+}
+
+
+function animateLeft(obj, from, to){
+  //console.log("animateleft");
+  obj.style.left = from+"vw";
+  if (to<0) {
+    if (from > to) {
+      setTimeout(function(){
+        animateLeft(obj, from - 1, to);
+      }, 3)
+    }
+  }
+  if (to >= 0) {
+    if (from < to) {
+      setTimeout(function(){
+        animateLeft(obj, from + 1, to);
+      }, 3)
+    }
+  }
+}
+
+
 function enableFullPage() {
+  window.fltVisible=1;
+  var theotab = document.getElementById("fltLabelsButton").firstChild;
+  theotab.style.position = "relative";
+  recurseDomChildren(theotab, "relative", "block");
+
   document.getElementById("user-bar").style.display = "none";
   document.getElementsByTagName("header")[0].style.display = "none";
   document.getElementsByTagName("footer")[0].style.display = "none";
@@ -502,29 +609,26 @@ function enableFullPage() {
       window.cadreExtWidth = b[i].style.width;
       b[i].style.width = "100vw";
       window.tlHautHeight = b[i].children[2].style.height;
-      b[i].children[2].style.height = "85vh";
+      b[i].children[2].style.height = "90vh";
       window.tlHautPos = b[i].children[2].style.position;
       b[i].children[2].style.position = "relative";
       window.tlBasTop = b[i].children[4].style.top;
       b[i].children[4].style.top = "0";
       window.tlBasHeight = b[i].children[4].style.height;
-      b[i].children[4].style.height = "15vh";
+      b[i].children[4].style.height = "10vh";
       window.tlBasPos = b[i].children[4].style.position;
       b[i].children[4].style.position = "relative";
     }
   }
 
   if (window.divfp != null) {
-    //console.log("avant delete "+window.divfp);
     window.divfp.parentNode.removeChild(window.divfp);
-    //console.log("après delete "+window.divfp);
   }
   window.divfp = window.tl.getDocument().createElement("div");
+  //window.divfp = document.createElement("div");
   window.divfp.className = "fullPage";
   delete window.fpimg;
-  //console.log(window.fpimg);
   window.fpimg = document.createElement('img');
-  //console.log(window.fpimg);
   window.fpimg.setAttribute('src', '/omeka-s/modules/Timeline/asset/img/exit-fullscreen_white.svg');
   window.divfp.appendChild(window.fpimg);
   window.fpimg.style.marginRight="0.15em";
@@ -532,13 +636,71 @@ function enableFullPage() {
   window.fpimg.onclick = function() {disableFullPage();}; // for IE
   window.tl._containerDiv.appendChild(window.divfp);
   window.divfp.style.zIndex="100";
-  //window.divfp.style.position = "relative";
-  //window.divfp.style.float = "right";
-  //window.divfp.style.bottom = "100%"
   window.divfp.style.position = "absolute";
   window.divfp.style.right = "0%";
   window.divfp.style.bottom = "0%";
   window.divfp.style.cursor = "pointer";
+
+  window.divfpfiltzone = window.tl.getDocument().createElement("div");
+  var divFleche = document.createElement('div');
+  window.fleche = document.createElement('img');
+  //window.divfpfiltzone = document.createElement("div");
+  window.divfpfiltzone.id = "fullPageFiltersZone";
+  window.divfpfiltzone.style.position = "absolute";
+  window.divfpfiltzone.style.top = "0%";
+  window.divfpfiltzone.style.zIndex="101";
+  window.divfpfiltzone.style.height = "100vh";
+  if (screen.height < 1080) {
+    window.divfpfiltzone.style.width = "26vw";
+    divFleche.style.height = "5vh";
+    animateLeft(window.divfpfiltzone, 0, -24);
+    window.fleche.setAttribute('src', '/omeka-s/modules/Timeline/asset/img/flecheDroite.svg');
+    window.fltVisible = 0;
+
+  }
+  else {
+    window.divfpfiltzone.style.width = "19vw";
+    divFleche.style.height = "4vh";
+    animateLeft(window.divfpfiltzone, 0, -17);
+    window.fleche.setAttribute('src', '/omeka-s/modules/Timeline/asset/img/flecheDroite.svg');
+    window.fltVisible = 0;
+  }
+  window.divfpfilt = window.tl.getDocument().createElement("div");
+  window.divfpfilt.id = "fullPageFilters";
+  window.divfpfilt.style.width = "90%";
+  window.divfpfilt.style.height = "100vh";
+  window.divfpfilt.style.position = "absolute";
+  window.divfpfilt.style.top = "0%";
+  window.divfpfilt.style.left = "0%";
+  window.divfpfilt.style.backgroundColor = "rgb(97, 87, 107)";
+  window.divfpfilt.style.zIndex="101";
+  window.divfpfilt.style.overflow = "auto";
+  window.divfpfilt.appendChild(document.getElementById("filters"));
+  window.divfpfilt.firstChild.style.display = "block";
+  window.divfpfilt.style.color = "white";
+  window.divfpfiltzone.appendChild(window.divfpfilt);
+  window.tl._containerDiv.appendChild(divfpfiltzone);
+
+  divFleche.id = "divFleche";
+  divFleche.appendChild(fleche);
+  divFleche.style.position = "absolute";
+  divFleche.style.width = "10%";
+  divFleche.style.right = "0%";
+  divFleche.style.top = "47%";
+  divFleche.style.zIndex = "101";
+  divFleche.style.backgroundColor = "rgb(97, 87, 107)";
+  /*
+  divFleche.addEventListener("mouseover", function(event) {
+    event.target.style.display = "block";
+  });
+  divFleche.addEventListener("mouseleave", function(event) {
+    event.target.style.display = "none";
+  });
+  */
+  window.divfpfiltzone.appendChild(divFleche);
+
+  window.fleche.setAttribute('onclick','clickfleche();'); // for FF
+  window.fleche.onclick = function() {clickfleche();}; // for IE
 }
 
 function disableFullPage() {
@@ -566,5 +728,8 @@ function disableFullPage() {
       b[i].children[4].style.position = window.tlBasPos;
     }
   }
+  document.getElementsByClassName("blocks")[0].appendChild(document.getElementById("filters"));
+  recurseDomChildren(document.getElementsByClassName("blocks")[0], "", "");
+  window.divfpfilt.parentNode.removeChild(window.divfpfilt);
   addFullPageButton(window.tlid);
 }
